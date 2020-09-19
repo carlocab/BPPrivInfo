29c82cad5cc1a61a13520f597faa0bfd33809f50
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2020-09-20 00:11:28 +0100

    Update plotting script

--- scripts/onecut.jl
+++ scripts/onecut.jl
@@ -4,19 +4,21 @@ using BPPrivInfo
 using Distributions
 using Plots
 using LaTeXStrings
+using QuadGK
 
 export plotfuncs
 
-lb = 0.0
-ub = 0.5
-dist = TriangularDist(lb, ub) 
+const lb = 0.0
+const ub = 0.5
+const tridist = TriangularDist(lb, ub) 
+const sin_f = t -> sin(4π * t) + 1
 
-function plotfuncs(n=501, dist=dist)
+function plotfuncs(n=501, dist::Distribution=tridist)
     x = range(minimum(dist), maximum(dist), length = n)
-    y₁ = map(V, x)
-    y₂ = map(t -> -μ(t), x)
-    y₃ = map(μ, x)
-    y₄ = map(λ, x)
+    y₁ = map(t -> V(t, dist), x)
+    y₂ = map(t -> -μ(t, dist), x)
+    y₃ = map(t -> μ(t, dist), x)
+    y₄ = map(t -> λ(t, dist), x)
     plt = plot(x, [y₁ y₂ y₃ y₄], labels = [L"V" L"V^\prime" L"\mu" L"\lambda"])
     hline!([0.0], label = L"0")
     xlabel!("Cutoff")
@@ -24,4 +26,17 @@ function plotfuncs(n=501, dist=dist)
     return plt
 end
 
+function plotfuncs(n, f::Function, lb::Real=lb, ub::Real=ub)
+    x = range(lb, ub, length = n)
+    y₁ = map(t -> V(t, f, ub), x)
+    y₂ = map(t -> -μ(t, f, ub), x)
+    y₃ = map(t -> μ(t, f, ub), x)
+    y₄ = map(t -> λ(t, f, ub), x)
+    plt = plot(x, [y₁ y₂ y₃ y₄], labels = [L"V" L"V^\prime" L"\mu" L"\lambda"])
+    hline!([0.0], label = L"0")
+    xlabel!("Cutoff")
+    plot!(legend = :outertopright)
+    return plt
 end
+
+end # module
