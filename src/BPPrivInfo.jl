47b1f36cb713d82e7161a2058279083720cbb773
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2020-09-19 21:03:37 +0100

    Clean up function args, add docstrings

--- src/BPPrivInfo.jl
+++ src/BPPrivInfo.jl
@@ -1,6 +1,5 @@
 module BPPrivInfo
 
-using Plots
 using LaTeXStrings
 using Optim
 using QuadGK
@@ -10,7 +9,7 @@ using Quadrature
 using ForwardDiff
 using Zygote
 
-export integrated_payoff, expected_payoff, V, λ, Λ, μ, dμdp
+export integrated_payoff, expected_payoff, V, λ, up_integral_λ, Λ, μ, dμdp
 
 const derivative = ForwardDiff.derivative
 
@@ -34,27 +33,45 @@ end
 
 # Sender Expected Payoffs
 payoff_integrand(type, cutoff, density) = payoff(type, cutoff) * density(type)
-integrated_payoff(cutoff, density=f, ub=ub) = quadgk(
-                                                t -> payoff_integrand(t, cutoff, density),
-                                                cutoff,
-                                                ub
-                                               )
-expected_payoff(cutoff, density=f, ub=ub) = inteated_payoff(cutoff, density, ub)[1]
-V(cutoff, f=f, ub=ub) = expected_payoff(cutoff, f, ub)
+integrated_payoff(cutoff, density=f) = quadgk(
+                                              t -> payoff_integrand(t, cutoff, density),
+                                              cutoff,
+                                              maximum(density)
+                                             )
+"""
+Sender expected payoff as a function of the cutoff and the type density f
+"""
+expected_payoff(cutoff, density=f) = inteated_payoff(cutoff, density)[1]
+"""
+Alias for expected_payoff
+"""
+V(cutoff, f=f) = expected_payoff(cutoff, f)
 
 # Lagrange Multipliers
 series_integrand(t, f) = (1 - t) * f(t)
-series_integral(p, f, ub) = quadgk(t -> series_integrand(t, f), p, ub)
-integral_term(p, f, ub) = series_integral(p, f, ub)[1]
+series_integral(p, f) = quadgk(t -> series_integrand(t, f), p, maximum(f))
+integral_term(p, f) = series_integral(p, f)[1]
 
 # Multiplier for IC constraint
-λ(p, f=f, ub=ub) = f(p) - integral_term(p, f, ub)
-up_integral_λ(p, f, ub) = quadgk(t -> λ(t, f, ub), p, ub)
-Λ(p, f=f, ub=ub) = up_integral_λ(p, f, ub)[1]
+"""
+Lagrange multiplier for the IC (reporting) constraint
+"""
+λ(p, f=f) = f(p) - integral_term(p, f)
+up_integral_λ(p, f=f) = quadgk(t -> λ(t, f), p, maximum(f))
+"""
+``\\Lambda (p, f) = \\int_p^{1/2} \\lambda(t, f) \\, \\mathrm{d}t``
+"""
+Λ(p, f=f) = up_integral_λ(p, f)[1]
 
 # Multiplier for bound constraint on π_G
+"""
+``\\mu`` is the Lagrange multiplier for the constraint that ``\\pi_G`` must be a probability.
+"""
 μ(p, f=f, ub=ub) = 2p * f(p) - integral_term(p, f, ub)
 # ∂μ/∂p
+"""
+``\\partial \\mu / \\partial p``
+"""
 dμdp(p, f=f, ub=ub) = (2 + 1 / (1 - p)) * f(p) +
                     2p * derivative(f, p) +
                     2integral_term(p, f, ub) / (1 - p)
