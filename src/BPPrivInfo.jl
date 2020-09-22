6dc1b47a7369d92d55ee91cf833b0a84dd1e378a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2020-09-22 17:42:34 +0100

    Make `V` consistent with `expected_payoff`
    `V` is an alias for `expected_payoff`, so they should have the same
    signature.

--- src/BPPrivInfo.jl
+++ src/BPPrivInfo.jl
@@ -79,7 +79,7 @@ end
 """
 Alias for expected_payoff
 """
-V(cutoff::Real, dist::CUD=unidist, n::Integer=500) = expected_payoff(cutoff, dist; n = n)
+V(cutoff::Real, dist::CUD=unidist; n::Integer=500) = expected_payoff(cutoff, dist; n = n)
 V(cutoff, f::Function, ub::Real) = expected_payoff(cutoff, f, ub)
 V(cutoff, density::Density=unidens) = V(cutoff, density.f, density.ub)
 V(cutoff, dist::DiscreteNonParametric) = expected_payoff(cutoff, dist)
